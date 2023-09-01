88f6edec28c11156ab65eccf2f0b3fec6015d28f
Remi <taniel.remi@gmail.com>
2023-09-01 07:31:37 +0200

    🎉 template driven

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+    standalone: true,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [FormsModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+})
+export class AppComponent {
+    save(value: any): void {
+        console.info('value', value);
+    }
+}
