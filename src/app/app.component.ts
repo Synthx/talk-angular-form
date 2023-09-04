5b4ed9332876d09cec15fcf0db14d05fb20428d4
Remi <taniel.remi@gmail.com>
2023-09-04 08:28:59 +0200

    ✨ custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,29 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+
+function shouldStartWithTest(): ValidatorFn {
+    return control => {
+        if (!control.value.startsWith('test')) {
+            return { shouldStartWithTest: true };
+        }
+
+        return null;
+    };
+}
 
 @Component({
     standalone: true,
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+    imports: [ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatInputModule, NgIf],
 })
 export class AppComponent {
-    formGroup = this.fb.group({
-        email: '',
-        password: '',
-        nickname: '',
-    });
+    control = this.fb.control('', [Validators.required, shouldStartWithTest()]);
 
     constructor(private readonly fb: FormBuilder) {}
-
-    save(): void {
-        console.info('value', this.formGroup.value);
-    }
 }
